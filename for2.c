d5e8c6960b4d64f388c7214ef039b64809f5f93f
김준우 <k47682441@gmail.com>
2025-06-14 18:09:04 +0900

    Create for2.c
    기초적인 for문 사용

---
+++ for2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("* ");
+    }
+    return 0;
+}
