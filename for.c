1f47aa2d7ea082d980736ccc1418129cb7a4b053
김준우 <k47682441@gmail.com>
2025-06-14 18:07:44 +0900

    Create for.c
    간단한 for문 사용

---
+++ for.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+int i;
+for(i=0;i < 1; i++){
+    printf("* ");
+}
+printf("\n");
+for(i=0;i < 2; i++){
+    printf("* ");
+}
+printf("\n");
+for(i=0;i < 3; i++){
+    printf("* ");
+}
+printf("\n");
+for(i=0; i < 4; i++){
+    printf("* ");
+}
+printf("\n");
+for(i=0;i < 5; i++){
+    printf("* ");
+}
+return 0;
+}
